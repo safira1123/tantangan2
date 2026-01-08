d2cd05c248533e293e93387c0f7a82c4825b6f0e
safira1123 <safirarahmatillah06@gmail.com>
2026-01-08 19:46:44 +0700

    biodata

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Mendefinisikan variabel
+$nama = "Andi";
+$tahun_lahir = 2005;
+$tahun_sekarang = 2026;
+
+// Menghitung umur
+$umur = $tahun_sekarang - $tahun_lahir;
+
+// Menampilkan output
+echo "Nama saya adalah $nama <br>";
+echo "Saya lahir tahun $tahun_lahir <br>";
+echo "Umur saya sekarang adalah $umur tahun";
+?>
